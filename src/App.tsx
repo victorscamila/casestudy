a7cd651625ac76f4efbe2d4e12775ee85198bde0
camila <camila@leanonsystems.com>
2023-11-16 19:36:38 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <>
+      <main>
+        <h1> tipo black storys</h1>
+        <nav>
+          <button>Criar nova carta</button>
+          <button>Puxar carta aleatoria</button>
+        </nav>
+      </main>
+    </>
+  )
+}
+
+export default App
